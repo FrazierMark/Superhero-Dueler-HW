8c4af3bfdcef33e429d1e24394bcecc1e4a7c9ca
FrazierMark <m.mark.frazier@gmail.com>
2023-10-26 10:25:13 -0700

    created dog class

---
+++ dog.py
@@ -0,0 +1,19 @@
+# dog.py
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("dog initialized!")
+
+    def bark(self):
+        print("Woof!")
+        
+    def sit(self):
+        print(self.name + " sits")
+        
+    def roll_over(self):
+        print(self.name + " rolls over")
+        
+        
+    
+    
---
+++ my_dogs.py
@@ -0,0 +1,20 @@
+from dog import Dog
+
+
+my_dog = Dog("Rex", "SuperDog")
+
+my_dog.bark()
+
+
+my_other_dog = Dog("Annie", "SuperDog")
+print(my_other_dog.name)
+
+
+best_dog = Dog("Marshall", "Great Dog")
+
+great_dog = Dog("Pepper Ann", "Best Dog")
+
+
+best_dog.sit()
+great_dog.roll_over()
+
